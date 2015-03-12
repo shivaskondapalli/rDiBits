47210232a2dc8e39fb2f61b4f3abab8aab9325d8
shivaskondapalli <shivaskondapalli@gmail.com>
2015-03-12 17:09:01 -0600

    Modified dataOps.R

--- dataOps.R
+++ dataOps.R
@@ -11,18 +11,26 @@ dataOps <- function(dataFile) {
   # Get column names
   strColNames <- names(dataFrame)
   
-  ## Count number of NAs  
-  # Variable to store number of NAs
-  numOfNAs = 0
-  
-  for (iCol in 1:nc) {
-    for(iRow in 1:nr) {
-      if(is.na(dataFrame[iRow, iCol])) {
-        numOfNAs = numOfNAs + 1
-      }
-    }
-  }
-  
- print (numOfNAs)
-  
+#   ## Count number of NAs  
+#   # Variable to store number of NAs
+#   numOfNAs = 0
+#   
+#   for (iCol in 1:nc) {
+#     for(iRow in 1:nr) {
+#       if(is.na(dataFrame[iRow, iCol])) {
+#         numOfNAs = numOfNAs + 1
+#       }
+#     }
+#   }
+# 
+#   sprintf ("Number of NAs: %d", numOfNAs)
+
+    ## Get mean of each column eliminating NA
+   for (iCol in 1:nc) {
+      colMean <- mean(dataFrame[, iCol], na.rm = TRUE)
+      #sprintf("Mean for %f is %f", iCol, colMean)
+      print (strColNames[iCol])
+      print (colMean)
+   }
+
 }
